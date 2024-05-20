6f875b7231b98053d82edaec88e66fbe922761a1
BeataKostkowska <kostkowska.beata@gmail.com>
2024-05-20 16:00:02 +0200

    added bootstrap buttons for voting

--- src/components/VotingButtons.jsx
+++ src/components/VotingButtons.jsx
@@ -1,3 +1,5 @@
+import Button from 'react-bootstrap/Button';
+
 export function VotingButtons ({upvotes, downvotes, mem, setMemesList, memesList}) {
 
     const upVotesCounter = (mem) => {
@@ -16,8 +18,10 @@ export function VotingButtons ({upvotes, downvotes, mem, setMemesList, memesList
 
     return (
         <div>
-            <button onClick={() => upVotesCounter(mem)}>{upvotes}</button>
-            <button onClick={() => downVotesCounter(mem)}>{downvotes}</button>
+            <Button onClick={() => upVotesCounter(mem)} variant="outline-success">Upvotes: {upvotes}</Button>
+            <Button onClick={() => downVotesCounter(mem)} variant="outline-danger">Downvotes: {downvotes}</Button>
+            {/* <button onClick={() => upVotesCounter(mem)} type="button" className="btn btn-outline-success" >{upvotes}</button> */}
+            {/* <button onClick={() => downVotesCounter(mem)} type="button" className="btn btn-outline-danger" >{downvotes}</button> */}
             <p>Różnica głosów {upvotes+downvotes}</p>
         </div>
     );
